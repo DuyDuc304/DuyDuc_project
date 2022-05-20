4767cc46e944c548cbb5aea7e250f7c60fb2322f
DuyDuc304 <ducpy30042000@gmail.com>
2022-05-20 17:06:27 +0700

    thu6

--- DuyDuc_project/src/components/dashboard/calenda/calenda.tsx
+++ DuyDuc_project/src/components/dashboard/calenda/calenda.tsx
@@ -1,13 +1,14 @@
-import React from "react"
+import React from "react";
 
 
 class Calenda extends React.Component {
     render() {
         return (
-            < div >
 
-            </div >
-        )
+            <div></div>
+        );
     }
+
 }
+
 export default Calenda
