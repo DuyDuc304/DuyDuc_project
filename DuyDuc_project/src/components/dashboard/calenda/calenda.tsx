2aa63d89b579492412822c184edbd1892d5a5564
DuyDuc304 <ducpy30042000@gmail.com>
2022-05-20 17:34:30 +0700

    thu6

--- DuyDuc_project/src/components/dashboard/calenda/calenda.tsx
+++ DuyDuc_project/src/components/dashboard/calenda/calenda.tsx
@@ -1,19 +1,10 @@
-import React from "react";
-import { Calendar } from 'antd';
-import type { CalendarMode } from 'antd/lib/calendar/generateCalendar';
-import type { Moment } from 'moment';
-import './calenda.css';
+import React from 'react';
+
 const Calenda: React.FC = () => {
-    const onPanelChange = (value: Moment, mode: CalendarMode) => {
-        console.log(value.format('YYYY-MM-DD'), mode);
-    };
 
     return (
-        <div className="site-calendar-demo-card">
-            <Calendar fullscreen={false} onPanelChange={onPanelChange} />
-        </div>
+        <div></div>
     );
 };
 
-
-export default Calenda
+export default Calenda;
