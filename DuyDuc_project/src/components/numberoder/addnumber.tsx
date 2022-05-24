9039176dbd3afa5988666e6505ffdba99358c562
DuyDuc304 <ducpy30042000@gmail.com>
2022-05-24 23:56:10 +0700

    t3

--- DuyDuc_project/src/components/numberoder/addnumber.tsx
+++ DuyDuc_project/src/components/numberoder/addnumber.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menubar } from '../layout/menubar';
 import { Topbar } from '../layout/topbar';
 
 
-class AddNumber extends React.Component {
+class AddNumber extends React.Component<any, any> {
+
     render() {
         return (
             <div className='bg'>
                 <div className="Backgroundapp">
+                    <div className='addnewnum'>
+                        <div className='addnewnum-col'>
+
+                            <p className='addnewnum-title'>Cấp số mới </p>
+                            <p className='addnewnum-cdv'>Dịch vụ khách hàng lựa chọn</p><form >
+                                <select className='addnewnum-select-dv' name='TenDichVu' >
+                                    <option value="">Chọn dịch vụ</option>
+                                    <option value="khamTongQuan">Khám tổng quát</option>
+                                    <option value="KhamPhuSan">Khám phụ sản khoa</option>
+                                    <option value="KhamRangHamMat">Khám răng hàm mặt</option>
+                                </select>
+                                <div className='rowbtn' >
+                                    <a href="/login" className='button-huy' style={{ width: '115px' }} > Hủy bỏ</a>
+                                    <button className='myform-button ' type='submit' style={{ width: '115px' }}>In số</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                    <div className='title-report-type'>Quản lý cấp số</div>
                     <Topbar nametitle1='Cấp số ﹥ ' nametitle2='Danh sách cấp số ﹥ ' nametitle3='Thêm số mới' href='/NumberOrder' href2='' nametitle21='' />
                     <Menubar buttonid="cs" />
 
--- DuyDuc_project/src/components/numberoder/numberorder.tsx
+++ DuyDuc_project/src/components/numberoder/numberorder.tsx
@@ -3,12 +3,29 @@ import React from 'react';
 import '../layout/menubar';
 import { Topbar } from '../layout/topbar';
 import { Menubar } from '../layout/menubar';
-import { AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlinePlus, AiOutlineSearch } from 'react-icons/ai';
 import TableNumber from './table-number';
 import Nextpage from '../layout/nextpage';
+import { FaRegCalendarAlt } from 'react-icons/fa';
+import Calendar from 'react-calendar';
 
+let newDate = new Date()
+let day = newDate.getDate();
+let month = newDate.getMonth() + 1;
+let year = newDate.getFullYear();
+let setday = day + "/" + month + "/" + year;
 
 class NumberOrder extends React.Component {
+    OnClick() {
+        const cld: any = document.querySelector('.daytime-picker-caledar-qlcs')
+        if (cld.style.display == "none") {
+            cld.style.display = "block";
+        } else {
+            cld.style.display = 'none';
+        }
+
+
+    }
     render() {
         return (
             <div className='bg'>
@@ -47,11 +64,38 @@ class NumberOrder extends React.Component {
                                 <option value="unconect">Tổng quát</option>
                             </select>
                         </div>
+                        <div className='qlcs-tdv'>
+                            <p>Chọn thời gian</p>
+                            <div className='ctg-row '>
+                                <div className='daytime-picker-qlcs' onClick={this.OnClick} >
+                                    <FaRegCalendarAlt className='icon-calendar' />
+                                    <p>{setday}</p>
+                                </div>
+                                <p style={{ color: '#535261', margin: '10px', display: 'flex' }} >▸</p>
+                                <div className='daytime-picker-qlcs' >
+                                    <FaRegCalendarAlt className='icon-calendar' />
+                                    <p>{setday}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='qlcs-tdv'>
+                            <p >Từ khóa</p>
+                            <div className='sreach-qlcs' >
+                                <form  >
+                                    <input type="text" name='timkiem' placeholder="Nhập từ khóa ..." style={{ fontSize: '14px' }} ></input>
+                                    <button className="sreach-sv-button" type='submit'><AiOutlineSearch /></button>
+                                </form>
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <TableNumber />
+                    <div className='daytime-picker-caledar-qlcs' id='cldsv'>
+                        <Calendar locale='en' selectRange />
                     </div>
 
 
-
-                    <TableNumber />
                     <div className='title-report-type'>Quản lý cấp số</div>
                     <Nextpage />
                     <Topbar nametitle1='Cấp số ﹥ ' nametitle2='' nametitle3='Danh sách cấp số' href='' href2='' nametitle21='' />
