ed1f3a0f236bfabc046a893ae78376d635d48814
DuyDuc304 <ducpy30042000@gmail.com>
2022-06-03 12:20:33 +0700

    t6

--- DuyDuc_project/src/components/layout/nextpage.tsx
+++ DuyDuc_project/src/components/layout/nextpage.tsx
@@ -5,15 +5,15 @@ class Nextpage extends React.Component {
     render() {
         return (
             <div className="row-nextpage">
-                <div className="page-number" ><p>◀</p></div>
-                <div className="page-number-onclick" id="1" ><p>1</p></div>
+                <div className="page-number" id="prev" ><p>◀</p></div>
+                <div className="page-number page-number-onclick" id="1" ><p>1</p></div>
                 <div className="page-number" id="2" ><p>2</p></div>
                 <div className="page-number" id="3"><p>3</p></div>
                 <div className="page-number" id="4" ><p>4</p></div>
                 <div className="page-number" id="5" ><p>5</p></div>
                 <div className="page-number" id="..." ><p>...</p></div>
                 <div className="page-number" id="10"><p>10</p></div>
-                <div className="page-number"><p>▶</p></div>
+                <div className="page-number" id="next"><p>▶</p></div>
             </div>
         )
     }
